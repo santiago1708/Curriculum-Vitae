7cdcd4aeaa8eb9f2ef12233b226dd0a568983984
santiago1708 <vela123tique@gmail.com>
2024-09-01 18:32:16 -0500

    Tercer commit: Se mejoro el Js y se agrego Responsive, ademas se termino el HTMl

--- js/app.js
+++ js/app.js
@@ -6,19 +6,36 @@ function mostrarVentana() {
     const sobremi = document.querySelector('#contenido_sobreMi')
     const resumen = document.querySelector('#contenido_resumen')
     const enlaces = document.querySelectorAll('.navegacion-principal a')
+    mostrarContenidoInicial();
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', (event) => {
+            event.preventDefault();
 
-    enlaces.forEach((ind) => {
-        ind.addEventListener('click', (event) => {
-            event.preventDefault()
-
-            sobremi.style.display = 'none'
-            resumen.style.display = 'none'
-
-            if (ind.classList.contains('sobremi')) {
+            sobremi.style.display = 'none';
+            resumen.style.display = 'none';
+            enlaces.forEach(link => link.classList.remove('active'));
+            enlace.classList.add('active');
+            if (enlace.classList.contains('sobremi')) {
                 sobremi.style.display = 'block';
-            } else if (ind.classList.contains('resumen')) {
+            } else if (enlace.classList.contains('resumen')) {
                 resumen.style.display = 'block';
             }
-        }) 
-    })
+        });
+    });
+}
+
+function mostrarContenidoInicial() {
+    const sobremi = document.querySelector('#contenido_sobreMi')
+    const resumen = document.querySelector('#contenido_resumen')
+    const enlaces = document.querySelectorAll('.navegacion-principal a')
+
+    sobremi.style.display = 'block';
+    resumen.style.display = 'none';
+    enlaces.forEach(enlace => {
+        if (enlace.classList.contains('sobremi')) {
+            enlace.classList.add('active');
+        } else {
+            enlace.classList.remove('active');
+        }
+    });
 }
